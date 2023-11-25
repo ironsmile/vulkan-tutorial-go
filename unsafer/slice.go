41f211f927574b41911e03a850d68b140f2ffa38
Doychin Atanasov <ironsmile@gmail.com>
2023-11-25 16:23:46 +0200

    Document the "usafer" package

--- unsafer/slice.go
+++ unsafer/slice.go
@@ -6,6 +6,9 @@ import (
 )
 
 // ToBytes interprets an arbitrary input slice as a byte slice.
+//
+// Note that the returned slice points to the same underlying data in memory. It
+// does not make a copy.
 func ToBytes[T any](input []T) []byte {
 	header := *(*reflect.SliceHeader)(unsafe.Pointer(&input))
 	header.Len = int(unsafe.Sizeof(input[0])) * len(input)

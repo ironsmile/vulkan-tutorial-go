aa1be6c9343b2a322ee8270ee7195fc00ebe4aee
Doychin Atanasov <ironsmile@gmail.com>
2023-12-05 20:24:26 +0200

    Compile shaders with go:generate

--- shaders/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-glslc shader.frag -o frag.spv
-glslc shader.vert -o vert.spv
-glslc shader.comp -o comp.spv
---
+++ shaders/gen.go
@@ -0,0 +1,5 @@
+package shaders
+
+//go:generate glslc shader.frag -o frag.spv
+//go:generate glslc shader.vert -o vert.spv
+//go:generate glslc shader.comp -o comp.spv

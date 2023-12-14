41a2edf7fbbe14600087d44aa2197fa7c3e73c7e
Doychin Atanasov <ironsmile@gmail.com>
2023-12-14 19:30:32 +0200

    Add code for 01_instance_creation

---
+++ code/01_instance_creation/main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"runtime"
+
+	"github.com/go-gl/glfw/v3.3/glfw"
+	vk "github.com/vulkan-go/vulkan"
+)
+
+func init() {
+	// This is needed to arrange that main() runs on main thread.
+	// See documentation for functions that are only allowed to be called
+	// from the main thread.
+	runtime.LockOSThread()
+}
+
+const (
+	title = "Vulkan Tutorial: Instance"
+)
+
+func main() {
+	app := &VulkanTutorialApp{
+		width:  1024,
+		height: 768,
+	}
+	if err := app.Run(); err != nil {
+		log.Fatalf("ERROR: %s", err)
+	}
+}
+
+// VulkanTutorialApp is the first program from vulkan-tutorial.com
+type VulkanTutorialApp struct {
+	width  int
+	height int
+
+	window   *glfw.Window
+	instance vk.Instance
+}
+
+// Run runs the vulkan program.
+func (a *VulkanTutorialApp) Run() error {
+	if err := a.initWindow(); err != nil {
+		return fmt.Errorf("initWindow: %w", err)
+	}
+	defer a.cleanWindow()
+
+	if err := a.initVulkan(); err != nil {
+		return fmt.Errorf("initVulkan: %w", err)
+	}
+	defer a.cleanupVulkan()
+
+	if err := a.mainLoop(); err != nil {
+		return fmt.Errorf("mainLoop: %w", err)
+	}
+
+	return nil
+}
+
+func (a *VulkanTutorialApp) initWindow() error {
+	if err := glfw.Init(); err != nil {
+		return fmt.Errorf("glfw.Init: %w", err)
+	}
+
+	glfw.WindowHint(glfw.ClientAPI, glfw.NoAPI)
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+
+	window, err := glfw.CreateWindow(a.width, a.height, title, nil, nil)
+	if err != nil {
+		return fmt.Errorf("creating window: %w", err)
+	}
+
+	a.window = window
+	return nil
+}
+
+func (a *VulkanTutorialApp) cleanWindow() {
+	a.window.Destroy()
+	glfw.Terminate()
+}
+
+func (a *VulkanTutorialApp) initVulkan() error {
+	vk.SetGetInstanceProcAddr(glfw.GetVulkanGetInstanceProcAddress())
+
+	if err := vk.Init(); err != nil {
+		return fmt.Errorf("failed to init Vulkan Go: %w", err)
+	}
+
+	if err := a.createInsance(); err != nil {
+		return fmt.Errorf("createInstance: %w", err)
+	}
+
+	return nil
+}
+
+func (a *VulkanTutorialApp) cleanupVulkan() {
+	vk.DestroyInstance(a.instance, nil)
+}
+
+func (a *VulkanTutorialApp) createInsance() error {
+	appInfo := vk.ApplicationInfo{
+		SType:              vk.StructureTypeApplicationInfo,
+		PApplicationName:   title + "\x00",
+		ApplicationVersion: vk.MakeVersion(1, 0, 0),
+		PEngineName:        "No Engine\x00",
+		EngineVersion:      vk.MakeVersion(1, 0, 0),
+		ApiVersion:         vk.ApiVersion10,
+	}
+
+	glfwExtensions := glfw.GetCurrentContext().GetRequiredInstanceExtensions()
+	createInfo := vk.InstanceCreateInfo{
+		SType:                   vk.StructureTypeInstanceCreateInfo,
+		PApplicationInfo:        &appInfo,
+		EnabledExtensionCount:   uint32(len(glfwExtensions)),
+		PpEnabledExtensionNames: glfwExtensions,
+	}
+
+	var instance vk.Instance
+	if res := vk.CreateInstance(&createInfo, nil, &instance); res != vk.Success {
+		return fmt.Errorf("failed to create Vulkan instance: %w", vk.Error(res))
+	}
+
+	a.instance = instance
+	return nil
+}
+
+func (a *VulkanTutorialApp) mainLoop() error {
+	log.Printf("main loop!\n")
+
+	for !a.window.ShouldClose() {
+		glfw.PollEvents()
+	}
+
+	return nil
+}
